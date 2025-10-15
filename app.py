4f2aa202ac3e3bf4bbd8f235824e4fa9674661ee
Levi <levirs999@gmail.com>
2025-10-14 21:24:07 -0300

    Codigo em python finalizado

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
 
 import openpyxl
+from urllib.parse import quote
+import webbrowser
+from time import sleep
+
+webbrowser.open('https://web.whatsapp.com/')
+sleep(20)
 
 workbook = openpyxl.load_workbook('Clientes.xlsx')
 pagina_clientes = workbook['Contatos']
@@ -8,10 +14,11 @@ for linha in pagina_clientes.iter_rows(min_row=2):
     #nome, telefone
     nome = linha[0]
     telefone = linha[1].value
+
+    mensagem = f'Olá! {nome}'
     
-    link_mensagem_whatsapp = f'https://web.whatsapp.com/send?phone={telefone}'
+    link_mensagem_whatsapp = f'https://web.whatsapp.com/send?phone={telefone}&text={quote(mensagem)}'
+    webbrowser.open(link_mensagem_whatsapp)
     
+    input('')
     
-    
-    print(nome)
-    print(telefone)
